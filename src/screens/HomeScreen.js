cebee24d55cb77d34b7625bc55d9b0781562330a
raghav <raghavjindal0212@gmail.com>
2022-04-13 11:30:44 +0530

    initial

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,25 +1,39 @@
-import React from 'react'
-import { Col, Row } from 'react-bootstrap'
-import Product from '../components/Product'
-import products from '../products'
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Col, Row } from "react-bootstrap";
+import Product from "../components/Product";
+import { listProducts } from "../actions/productActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const HomeScreen = () => {
-    return (
-        <>
-            <h1>Latest Products</h1>
+   const dispatch = useDispatch();
+
+   const productList = useSelector((state) => state.productList);
+   const { loading, error, products } = productList;
+
+   useEffect(() => {
+      dispatch(listProducts());
+   }, [dispatch]);
+
+   return (
+      <>
+         <h1>Latest Products</h1>
+         {loading ? (
+            <Loader />
+         ) : error ? (
+            <Message variant="danger">{error}</Message>
+         ) : (
             <Row>
-                {
-                    products.map((product) => (
+               {products.map((product) => (
+                  <Col key={product._id} sm={12} md={4} lg={3}>
+                     <Product product={product} />
+                  </Col>
+               ))}
+            </Row>
+         )}
+      </>
+   );
+};
 
-                        <Col key={product._id} sm={12} md={6} lg={4}>
-                            <Product product={product}/> 
-                        </Col>
-                    ))
-                }
-            </Row>   
-        </>
-    )
-}
-
-export default HomeScreen
+export default HomeScreen;
